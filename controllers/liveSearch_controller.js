9f17d3125ab3b12c998bab4cb2777dfb84fffde6
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-03-12 20:50:57 -0400

    updaded the json package

--- controllers/liveSearch_controller.js
+++ controllers/liveSearch_controller.js
@@ -2,4 +2,14 @@ const express = require('express');
 const router = express.Router();
 const liveSearch = require('../models/liveSearch');
 
+router.get('/', (req, res) => {
+	liveSearch.all(function(data) {
+		let liveSearchObj = {
+			videos : data
+		};
+		console.log(liveSearchObj);
+		res.render('index', liveSearchObj);
+	});
+});
+
 module.exports = router;
