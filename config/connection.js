27c7f84fa187eb8c5c8608dcc7ef98c5f3f07141
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-03-12 21:01:44 -0400

    updated the connection file to require mysql

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql');
+
 let connection;
 
 if (process.env.JAWSDB_URL) {
@@ -11,20 +13,4 @@ if (process.env.JAWSDB_URL) {
 		database : 'burgers_db'
 	});
 }
-
-connection.js;
-
-if (process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {
-     connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "avengers878",
-        database: "burgers_db"
-    });
-}
-
-connection.js
-
+module.exports = connection;
