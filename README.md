7fd611df71c034631580a5057adb757e43e0f9ab
Austin Smith <austin.smith.ads@gmail.com>
2020-03-14 11:31:44 -0400

    fixed file structure

--- README.md
+++
@@ -1,6 +0,0 @@
-# Project-2
-
-Google Doc link:
-https://docs.google.com/document/d/1EWzecJmQfSEAxf_fRHad6PIZNQixZS4b-TFdbfeHxG0/edit?ts=5e682d58
-
-
--- server.js
+++ server.js
@@ -1,21 +1,30 @@
+// Requiring necessary npm packages
+var express = require("express");
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
 
-let express = require('express');
-let PORT = process.env.PORT || 8081;
-let app = express();
+// Setting up port and requiring models for syncing
+var PORT = process.env.PORT || 8080;
+var db = require("./models");
 
-app.use(express.static('public'));
-
+// Creating express app and configuring middleware needed for authentication
+var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-let exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
-let routes = require('./controllers/liveSearch_controller');
-
-app.use(routes);
-
-app.listen(PORT, () => {
-	console.log('Server listening on: http://localhost:' + PORT);
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  });
 });
