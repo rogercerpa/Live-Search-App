fa8797948a313360b48f9ba449226840a4809f16
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-03-16 23:04:09 -0400

    initial javascript code for livesearch results page, search bar code

--- public/js/livevideos.js
+++ public/js/livevideos.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.email);
-  });
+	const searchButton = $('form.topicSearch');
+	const userSearch = $('input#vsearch');
+
+	searchButton.on('submit', function(event) {
+		event.preventDefault();
+		const topic = {
+			search : userSearch.val().trim()
+		};
+		if (topic.search === sequelize) {
+			return;
+		}
+	});
+
+	// This file just does a GET request to figure out which user is logged in
+	// and updates the HTML on the page
+	$.get('/api/user_data').then(function(data) {
+		$('.member-name').text(data.email);
+	});
 });
