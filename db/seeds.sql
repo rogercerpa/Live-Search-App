f3d73118779b1b855ac81b41396e2557cd50e8ff
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-03-17 20:04:06 -0400

    added db, schema and seeds files with basic database

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+
+-- sequelize
+INSERT INTO sequelize
+    (activity_name)
+VALUES
+    ('03-Chirpy-Sequelize');
+
+
+-- express
+INSERT INTO express
+    (name)
+VALUES
+    ('');
+
+
+-- OOP
+INSERT INTO OOP
+    (name)
+VALUES
+    ('');
+
