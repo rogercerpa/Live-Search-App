6b8180359bacc86d81dc6976dc4aedbecfa88f4d
IV <irisviruet@hotmail.com>
2020-03-18 19:53:23 -0400

    removed syntax errors from schema.sql-gtg

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,20 @@
 
 -- sequelize
+
+INSERT INTO sequelize (activity_name, main_topic, time_location, 
+homework, hint_comment, folder) VALUES ('Setting up sequelize', 'Setting up sequelize', 
+'1:10', true, 'Shows how to set up sequelize', 
+'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/04-Supplemental');
+
+INSERT INTO sequelize (activity_name, main_topic, time_location, 
+homework, hint_comment, folder) VALUES ('Setting up sequelize on bash terminal.', 'Setting up sequelize on bash terminal.',
+'6:51', true, 'Commands on terminal to set up sequelize project', 
+);
+
+INSERT INTO sequelize (activity_name, main_topic, time_location, 
+homework, hint_comment, folder) VALUES ('07-ORM-To-Sequelize', 'Briefly going over object model of sequelize', 
+'8:34', false, 'Object model for sequelize', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/07-ORM-To-Sequelize');
+=======
 INSERT INTO sequelize
     (activity_name, main_topic, time_location,homework, hint_comment, folder )
 VALUES
@@ -15,6 +30,11 @@ port
 https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/11-express/01-Activities/04-Serving-HTML
 ');
 
+INSERT INTO sequelize (activity_name, main_topic, time_location, 
+homework, hint_comment, folder) VALUES ('03-Chirpy-Sequelize', 'Chirpy Sequelize', '12:31', false, 'How to manipulate/define sequelize objects', 
+'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/03-Chirpy-Sequelize' 
+)
+-- INSERT INTO sequelize ()
 
 -- express
 INSERT INTO express
@@ -24,15 +44,20 @@ VALUES
 
 
 -- OOP
-INSERT INTO OOP
-    (name)
+INSERT INTO OOP (activity_name, main_topic, time_location, homework, hint_comment, folder)
+    
 VALUES
-    ('');
+    ("17-Stu_Prototype_Review", "Update tv constructor function to get searched actors' names", "9:55", true, "Using axios package to search API, repeat function", "https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/10-OOP/01-Activities/17-Stu-Prototype-Review"),
+    ("18-Ins_Classes", "Using classes to structure code", "23:26", false, "classes can inherit properties from each other", "https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/blob/master/10-OOP/01-Activities/18-Ins-Classes/shape.js"),
+	("19-Stu_Classes", "Using classes to make characters fight", "46:00", true, "setInterval for fights, printstats() for battle visual", "https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/10-OOP/01-Activities/19-Stu-Classes"),
+	("20-Ins_Classes", "Classes inherit properties from parent class", "1:04:00", false, "Extends to inherit from parent class", "https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/10-OOP/01-Activities/20-Ins-Subclasses"),
+	("21-Stu-Subclasses", "Adding functionality to classes", "1:49:05", true, "Classes in OOP have ids", "https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/10-OOP/01-Activities/21-Stu-Subclasses"),
+	("22-Ins-Multiple Classes", "Making a class with a property that has multiple classes", "2:00:00", true, "Static properties", "https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/10-OOP/01-Activities/22-Ins-Multiple-Classes");
 
 -- Node.js
-INSERT INTO nodejs (activity_name, main_topic, time_location, homework, hint_comment, folder),
+INSERT INTO nodejs (activity_name, main_topic, time_location, homework, hint_comment, folder)
 VALUES
-    ("8-Stu_TDD_Dissect", "TDD and Jest", "37:36", false, "Using Jest to test" "https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/10-OOP/01-Activities/08-Stu_TDD-Dissect"),
+    ("8-Stu_TDD_Dissect", "TDD and Jest", "37:36", false, "Using Jest to test", "https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/10-OOP/01-Activities/08-Stu_TDD-Dissect"),
     ("9-STU_TDD", "Employing tdd before writing prototype methods", "1:08:00", false, ".prototype method", "https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/10-OOP/01-Activities/09-Stu_TDD"),
-    ("10-Stu_Pass_Tests", "Fully functional tests", "2:19:25", true, "helps with wk 10 hw", "https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/10-OOP/01-Activities/10-Stu_Pass-Tests")
+	("10-Stu_Pass_Tests", "Fully functional tests", "2:19:25", true, "helps with wk 10 hw", "https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/10-OOP/01-Activities/10-Stu_Pass-Tests")
  
