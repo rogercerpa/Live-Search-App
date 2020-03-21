3230aab865993b320bb555440b9e49e070f28ade
IV <irisviruet@hotmail.com>
2020-03-21 09:39:46 -0400

    server listening

--- models/expressTable.js
+++ models/expressTable.js
@@ -1,52 +1,34 @@
 module.exports = (sequelize, DataTypes) => {
+
 	const express = sequelize.define(
 		'express',
 		{
-			activity_name : {
-				type      : DataTypes.STRING,
-				allowNull : false
+			activity_name: {
+				type: DataTypes.STRING,
+				allowNull: false
 			},
-			main_topic    : {
-				type      : DataTypes.STRING,
-				allowNull : false
+			main_topic: {
+				type: DataTypes.STRING,
+				allowNull: false
 			},
-			time_location : {
-				type      : DataTypes.STRING,
-				allowNull : false
+			time_location: {
+				type: DataTypes.STRING,
+				allowNull: false
 			},
-			homework      : {
-				type : DataTypes.BOOLEAN
+			homework: {
+				type: DataTypes.BOOLEAN
 			},
-			hint_comment  : {
-				type      : DataTypes.STRING,
-				allowNull : false
+			hint_comment: {
+				type: DataTypes.STRING,
+				allowNull: false
 			},
-			folder        : {
-				type      : DataTypes.STRING,
-				allowNull : false
+			folder: {
+				type: DataTypes.STRING,
+				allowNull: false
 			}
 		},
 		{
-			freezeTableName : true
-		}
-	);
-
-<<<<<<< HEAD
-const express = sequelize.define("express",
-        {
-            activity_name: DataTypes.STRING,
-            allowNull: false,
-            main_topic: DataTypes.STRING,
-            time_location: DataTypes.STRING,
-            homework: DataTypes.BOOLEAN,
-            hint_comment: DataTypes.STRING,
-            folder: DataTypes.STRING,
-
-            freezeTableName: true
-        })
-return express;
-    };
-=======
+			freezeTableName: true
+		});
 	return express;
 };
->>>>>>> e5e59fbad39c48b527dee376adad717cf4fda187
