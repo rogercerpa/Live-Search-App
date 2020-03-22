89c91ae97de5c26b4acf18be27f6138702fdbff4
Austin Smith <austin.smith.ads@gmail.com>
2020-03-22 12:46:55 -0400

    sequelize table added with all info in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,63 +5,27 @@ INSERT INTO sequelize
     (activity_name, main_topic, time_location,
     homework, hint_comment, folder)
 VALUES
-    ('Setting up sequelize', 'Setting up sequelize',
-        '1:10', true, 'Shows how to set up sequelize',
-        'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/04-Supplemental');
-
-INSERT INTO sequelize
-    (activity_name, main_topic, time_location,
-    homework, hint_comment, folder)
-VALUES
-    ('Setting up sequelize on bash terminal.', 'Setting up sequelize on bash terminal.',
-        '6:51', true, 'Commands on terminal to set up sequelize project',  
-);
-
-INSERT INTO sequelize
-    (activity_name, main_topic, time_location,
-    homework, hint_comment, folder)
-VALUES
-    ('07-ORM-To-Sequelize', 'Briefly going over object model of sequelize',
-        '8:34', false, 'Object model for sequelize', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/07-ORM-To-Sequelize');
-
-INSERT INTO sequelize 
-    (activity_name, main_topic, time_location, homework, hint_comment, folder) 
-VALUES ('Setting up sequelize', 'Setting up sequelize', '1:10', true, 'Shows how to set up sequelize', 
-'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/04-Supplemental');
-
-INSERT INTO sequelize 
-    (activity_name, main_topic, time_location, homework, hint_comment, folder) 
-VALUES ('01-Chirpy_mySQL', 'Setting up sequelize on bash terminal.',
-'6:51', true, 'Commands on terminal to set up sequelize project', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/01-Chirpy-mySQL');
-
-INSERT INTO sequelize 
-    (activity_name, main_topic, time_location, homework, hint_comment, folder) 
-VALUES ('07-ORM-To-Sequelize', 'Briefly going over object model of sequelize', 
-'8:34', false, 'Object model for sequelize', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/07-ORM-To-Sequelize');
-
-
-INSERT INTO sequelize
-    (activity_name, main_topic, time_location,homework, hint_comment, folder )
-VALUES
-    ('First Server', 'Server, nodejs, Http library, port', '45:00', true, 'Setting up your first server, basic', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/11-express/01-Activities/01-FirstServer' ),
-	('Two Servers', 'Server, nodejs, Http library, port', '1:33:00', false, 'Shows how to have two server for you app', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/11-express/01-Activities/02-Two-Servers'),
-	('Serving HTML','Server, nodejs, http, html', '1:41:56', true, 'Showing information on the browser from the server', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/11-express/01-Activities/04-Serving-HTML');
-
-
-INSERT INTO sequelize
-    (activity_name, main_topic, time_location,
-    homework, hint_comment, folder)
-VALUES
-    ('03-Chirpy-Sequelize', 'Chirpy Sequelize', '12:31', false, 'How to manipulate/define sequelize objects',
-        'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/03-Chirpy-Sequelize' 
-)
--- INSERT INTO sequelize ()
-
-INSERT INTO sequelize (activity_name, main_topic, time_location, 
-homework, hint_comment, folder) 
-VALUES ('03-Chirpy-Sequelize', 'Chirpy Sequelize', '12:31', false, 'How to manipulate/define sequelize objects', 
-'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/03-Chirpy-Sequelize' 
-);
+    ('Setting up sequelize', 'Setting up sequelize', '1:10', true, 'Shows how to set up sequelize', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/04-Supplemental'),
+    ('Setting up sequelize on bash terminal.', 'Setting up sequelize on bash terminal.', '6:51', true, 'Commands on terminal to set up sequelize project', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/04-Supplemental'), 
+    ('07-ORM-To-Sequelize', 'Briefly going over object model of sequelize', '8:34', false, 'Object model for sequelize', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/07-ORM-To-Sequelize'),
+    ('03-Chirpy-Sequelize', 'Chirpy Sequelize', '12:31', false, 'How to manipulate/define sequelize objects', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/03-Chirpy-Sequelize'),
+    ('04-StarWars', 'Star Wars', '14:19', false, '.GET with specific parameters','https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/04-StarWars'),
+    ('07-ORM-To-Sequelize', 'To do list application', '16:00', true, 'starting to do list app', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master'),
+    ('07-ORM-To-Sequelize', '07-ORM-To-Sequelize', '22:15', false, 'Changing it so it works with Sequelize.  Deleting ORM methods and such.', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/07-ORM-To-Sequelize'),
+    ('07-ORM-To-Sequelize', '07-ORM-To-Sequelize', '27:13', true, 'Npm install both mysql2 and sequelize for sequelize to work', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/07-ORM-To-Sequelize'),
+    ('07-ORM-To-Sequelize', '07-ORM-To-Sequelize', '30:12', true, 'Make sure use “npm install -g sequelize sequelize-cli if package not found error(gets you global packages) use sequelize --version to make sure it’s installed', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/07-ORM-To-Sequelize'),
+    ('07-ORM-To-Sequelize', '07-ORM-To-Sequelize', '32:36', true, 'Make sure to use “sequelize init:models & sequelize init:config”These commands initialize models folder and config folder', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/07-ORM-To-Sequelize'),
+    ('07-ORM-To-Sequelize', '07-ORM-To-Sequelize', '36:23', true, 'Instructor talks about index.js file inside models folder.  Index.js file never needs to be edited.', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/07-ORM-To-Sequelize'),
+    ('07-ORM-To-Sequelize', '07-ORM-To-Sequelize', '41:26', true, 'How to define model in sequelize.', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/07-ORM-To-Sequelize'),
+    ('07-ORM-To-Sequelize', '07-ORM-To-Sequelize', '59:38', true, 'Setting up sequelize object(Tables).', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/07-ORM-To-Sequelize'),
+    ('07-ORM-To-Sequelize', '07-ORM-To-Sequelize', '1:01:18', true, 'Linking server.js to database', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/07-ORM-To-Sequelize'),
+    ('08-Sequelize-Create-Read', '08-Sequelize-Create-Read', '1:31:24', true, 'https://sequelize.org/v5/  Call db variables from defined sequelize object in todo.js example.', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/08-Sequelize-Create-Read'),
+    ('08-Sequelize-Create-Read', '08-Sequelize-Create-Read', '1:35:00', true, 'Get and post on api-routes.js  C.R of C.R.U.D', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/08-Sequelize-Create-Read'),
+    ('09-Sequelize-Update-Delete', '09-Sequelize-Update-Delete', '1:47:34', true, '.delete and .put  U.D of C.R.U.D', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/09-Sequelize-Update-Delete'),
+    ('09-Sequelize-Update-Delete', '09-Sequelize-Update-Delete', '2:29:31', true, 'Goes over .delete and .put for sequelize', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/09-Sequelize-Update-Delete'),
+    ('09-Sequelize-Update-Delete', '09-Sequelize-Update-Delete', '2:39:50', true, 'Recap of .delete .put', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/09-Sequelize-Update-Delete'),
+    ('09-Sequelize-Update-Delete', '09-Sequelize-Update-Delete', '2:59:52', true, 'Validation to prevent funky glitches on application using sequelize.define to create validations', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/09-Sequelize-Update-Delete'),
+    ('10-Sequelize-Validations', '10-Sequelize-Validations', '3:06:57', true, 'Using .catch(err => console.log(err)); to get rid of red validation error on terminal, just to make it look better when it outputs.', 'https://gt.bootcampcontent.com/GT-Coding-Boot-Camp/GT-ATL-FSF-PT-11-2019-U-C/tree/master/14-Full-Stack/01-Activities/10-Sequelize-Validations')
 
 
 -- SELECT * FROM sequelize;
